a490990355505c34ec948524d2b4a8177296984e
Grayson <grasnyder2001@gmail.com>
2024-09-18 16:49:05 -0500

    Added control for elbow from command Line

--- robotControl.py
+++ robotControl.py
@@ -24,11 +24,11 @@ robot = InterbotixManipulatorXS("px100", "arm", "gripper")
 robot_startup()
 mode = "h"
 
-# waistPos, shoulderPos, elbowPos, wristPos = updateJointPositions()
+waistPos, shoulderPos, elbowPos, wristPos = updateJointPositions()
 
 # Let the user select the position
 while mode != 'q':
-    mode=input("[h]ome, [s]leep, [o]pen grip, [c]lose grip, [r]otate, [u]p, [d]own, [q]uit ")
+    mode=input("[h]ome, [s]leep, [o]pen grip, [c]lose grip, [r]otate waist, [e]lbow move, [w]rist move, [q]uit ")
     if mode == "h":
         robot.arm.go_to_home_pose()
         waistPos, shoulderPos, elbowPos, wristPos = updateJointPositions()
@@ -40,13 +40,20 @@ while mode != 'q':
     elif mode == "o":
         robot.gripper.release()
     elif mode == "r":
-        waistChange = float(input("Input change in radians: "))
+        printMsg = 'Current Position is: %f' % wristPos
+        print(printMsg)
+        waistChange = float(input("Input change in radians (-Pi to Pi): "))
         if waistPos + waistChange < waistMax and waistPos + waistChange > waistMin:
             robot.arm.set_single_joint_position('waist', waistPos+waistChange)
             waistPos += waistChange
-    elif mode == "u":
-        pass
-    elif mode == "d":
-        pass
+    elif mode == "e":
+        printMsg = 'Current Position is: %f' % elbowPos
+        print(printMsg)
+        elbowChange = float(input("Input change in radians (APPROX. -2.11 to 1.60): "))
+        if elbowPos + elbowChange < elbowMax and elbowPos + elbowChange > elbowMin:
+            robot.arm.set_single_joint_position('elbow', elbowPos+elbowChange)
+            elbowPos += elbowChange
+    elif mode == "w":
+        print("Sorry not setup yet!")
 
 robot_shutdown()
