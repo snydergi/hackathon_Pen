5215a91d91513addd9d9e203571722c27b2db1ab
Grayson <grasnyder2001@gmail.com>
2024-09-21 11:14:48 -0500

    Working calibration and transformation. Applying solved rotation matrix (R) and translation vector (t) to camera point list from calibration results in list that is very close to robot point list from calibration, with noise

--- calibrate.py
+++ calibrate.py
@@ -28,14 +28,62 @@ def runCalibration():
 
         #Create one frame and add converted coordinate to cam point list
         cam.getOneConvertedFrame()
-        cameraPointList.append(cam.coordConverted)
+        if(cam.cx != 0 and cam.cy != 0):
+            cameraPointList.append(cam.coordConverted)
+
+    # print("Robot Points: ")
+    # print(robotPointList)
+    # print("Camera Points: ")
+    # print(cameraPointList)
+
+    camCx,camCy,camCz = findCentroid(cameraPointList)
+    robCx,robCy,robCz = findCentroid(robotPointList)
+
+    cameraPointsLessCentroid = subtractCentroid(camCx,camCy,camCz,cameraPointList)
+    robotPointsLessCentroid = subtractCentroid(robCx,robCy,robCz,robotPointList)
+
+    R_Mat, rssd = scipy.spatial.transform.Rotation.align_vectors(robotPointsLessCentroid, cameraPointsLessCentroid)
+
+    tVec = [robCx,robCy,robCz] - R_Mat.apply([camCx,camCy,camCz])
+
+    transformedCameraPoints = transformPoints(R_Mat,tVec,cameraPointList)
 
     print("Robot Points: ")
     print(robotPointList)
-    print("Camera Points: ")
-    print(cameraPointList)
+    print("Transformed Camera Points: ")
+    print(transformedCameraPoints)
 
     mrGrip.robotShutdown()
     cam.cleanup()
+
+def findCentroid(pointList):
+    x = 0
+    y = 0
+    z = 0
+    for pt in pointList:
+        x += pt[0]
+        y += pt[1]
+        z += pt[2]
+    x = (x / len(pointList))
+    y = (y / len(pointList))
+    z = (z / len(pointList))
+    return x,y,z
+
+def subtractCentroid(x,y,z,pointList):
+    newList = []
+    for pt in pointList:
+        xn = pt[0] - x
+        yn = pt[1] - y
+        zn = pt[2] - z
+        newList.append([xn,yn,zn])
+    return newList
+
+def transformPoints(R,t,pointsList):
+    newList = []
+    for pt in pointsList:
+        newPt = R.apply(pt) + t
+        newList.append(newPt)
+    return newList
+
 
 runCalibration()
